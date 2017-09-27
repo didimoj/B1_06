cfcb108437746bbbb0587bf5f7e861d1221578e5
didimoj <didimoj@hotmail.com>
2017-09-27 19:05:00 +0200

    Ya se puede leer el archivo

--- SistInteligentes/src/leerArchivo.java
+++ SistInteligentes/src/leerArchivo.java
@@ -8,15 +8,37 @@ import java.util.Scanner;
 public class leerArchivo {
 	public String cargarArchivo(String file) {
 		String s="";
+		int x, y, k, M, c, f;
 		try {
 			FileReader fr = new FileReader(new File(file));
 			Scanner pantalla = new Scanner(fr);
-			System.out.println(pantalla.next());
+			x=pantalla.nextInt();
+			y=pantalla.nextInt();
+			k=pantalla.nextInt();
+			M=pantalla.nextInt();
+			c=pantalla.nextInt();
+			f=pantalla.nextInt();
+			
+			int[][] terreno=new int[c][f];
+			for(int i=0;i<f;i++) {
+				for(int j=0;j<c;j++) {
+					terreno[i][j]=pantalla.nextInt();
+				}
+			}
+			imprimir(terreno);
 			
 		} catch (Exception e) {
 			System.out.println(e);
 			return null;
 		}
 		return s;
+	}
+	public static void imprimir(int[][] solar){
+		for(int i=0;i<solar.length;i++){
+			for(int j=0;j<solar.length;j++){
+				System.out.print(solar[i][j]+"\t");
+			}
+			System.out.println();
+		}
 	}
 }
