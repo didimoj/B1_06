284fcb8f1ed07f1178227751f3e9e9b66523d76b
didimoj <didimoj@hotmail.com>
2017-09-27 19:26:48 +0200

    Ya se lee y se escribe en el archivo

--- SistInteligentes/src/Main.java
+++ SistInteligentes/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		leerArchivo yo=new leerArchivo();
-		System.out.println("Hola");
+		System.out.println("Hola\nhola");
 		yo.cargarArchivo("terreno.txt");
 
 	}
--- SistInteligentes/src/leerArchivo.java
+++ SistInteligentes/src/leerArchivo.java
@@ -1,5 +1,8 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -26,12 +29,35 @@ public class leerArchivo {
 				}
 			}
 			imprimir(terreno);
-			
+			escribirArchivo("terreno.txt",0,3,3,11,3,3,terreno);
 		} catch (Exception e) {
 			System.out.println(e);
 			return null;
 		}
 		return s;
+	}
+	public void escribirArchivo(String path,int x,int y,int k, int M,int c,int f,int[][] terreno) {
+		File file=new File(path);
+		try {
+			FileWriter fw=new FileWriter(file,false);
+			BufferedWriter bw = new BufferedWriter(fw); 
+			//bw.newLine();
+			//bw.write("------------------");
+			//bw.newLine();
+			bw.write(x+" "+y+" "+k+" "+M+" "+c+" "+f+"\n ");
+			bw.newLine();
+			for(int i=0;i<f;i++) {
+				for(int j=0;j<c;j++) {
+					bw.write(" "+terreno[i][j]);
+				}
+				bw.newLine();
+			}
+			bw.close();
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public static void imprimir(int[][] solar){
 		for(int i=0;i<solar.length;i++){
