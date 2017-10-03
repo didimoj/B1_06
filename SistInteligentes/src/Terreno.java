61bf8c343c276a6ce4f63c77dced6e8492705128
Alberto Gómez <32299191+AlbertGomezL@users.noreply.github.com>
2017-10-03 14:38:40 +0200

    Agente corregido

--- SistInteligentes/src/Terreno.java
+++ SistInteligentes/src/Terreno.java
@@ -5,26 +5,31 @@ public class Terreno {
 	private int k;
 	private int max;
 	private int[][] terreno;
+
 	public Terreno(int x, int y, int k, int m, int[][] terreno) {
-		tractorX=x;
-		tractorY=y;
-		this.k=k;
-		max=m;
-		this.terreno=terreno;
-		
+		tractorX = x;
+		tractorY = y;
+		this.k = k;
+		max = m;
+		this.terreno = terreno;
 	}
+
 	public int tractorX() {
 		return tractorX;
 	}
+
 	public int tractorY() {
 		return tractorY;
 	}
+
 	public int K() {
 		return k;
 	}
+
 	public int Max() {
 		return max;
 	}
+
 	public int size() {
 		return terreno[0].length;
 	}
