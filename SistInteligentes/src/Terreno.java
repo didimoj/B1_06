3eb160838cb3fc3860423f81a4020dd1086ff270
Usuario <Usuario@172.19.212.78>
2017-10-03 10:13:40 +0200

    Corregido el agente

---
+++ SistInteligentes/src/Terreno.java
@@ -0,0 +1,15 @@
+
+public class Terreno {
+	public int tractorX() {
+		
+		return x;
+	}
+	public int tractorY() {
+		
+		return y;
+	}
+	public int size() {
+		
+		return 
+	}
+}
--- SistInteligentes/src/agente.java
+++ SistInteligentes/src/agente.java
@@ -1,27 +1,27 @@
 
 public class agente {
 	public static void combinaciones(Terreno t) {
-		if(t.tractorX() == 0 && t.tractorY()==0|| t.tractorX == 0 && t.tractorY== t.size() || (t.tractorX() == t.size && t.tractorY()==0|| t.tractorX == t.size() && t.tractorY== t.size()) {
+		if(t.tractorX() == 0 && t.tractorY()==0|| t.tractorX() == 0 && t.tractorY()== t.size() || t.tractorX() == t.size() && t.tractorY()==0|| t.tractorX() == t.size() && t.tractorY()== t.size()) {
 			//es decir si el tractor esta en una esquina del terreno
 			//si esta en una esquina solo habra dos sitios donde colocarlo
 			//hacer como se puede cojer y hacer las combinaciones de la cantidad menos k entre dos casillas y meterlas en una lista
 		}
-		else if(t.tractorX==0 && !t.tractorY==0 && !t.tractorY == t.size()) {
+		else if(t.tractorX()==0 && t.tractorY()!=0 && t.tractorY() != t.size()) {
 			//es decir, si el tractor esta en el borde izquierdo pero no en una esquina
 			//de esta forma tendria tres sitios donde colocar tierra
 			//hacer que se pueda cojer y las combinaciones
 		}
-		else if(t.tractorX==t.size() && !t.tractorY==0 && !t.tractorY == t.size()) {
+		else if(t.tractorX()==t.size() && t.tractorY()!=0 && t.tractorY() != t.size()) {
 			//es decir, si el tractor esta en el borde derecho pero no en una esquina
 			//de esta forma tendria tres sitios donde colocar tierra
 			//hacer que se pueda cojer y las combinaciones
 			
 			//lo diferencio para que sea automatico y mas facil de cojer luego
-		}else if(t.tractorY==0 && !t.tractorX==0 && !t.tractorX == t.size()) {
+		}else if(t.tractorY()==0 && t.tractorX()!=0 && t.tractorX() != t.size()) {
 			//es decir, si el tractor esta en el borde de arriba pero no en una esquina
 			//de esta forma tendria tres sitios donde colocar tierra
 			//hacer que se pueda cojer y las combinaciones
-		}else if(t.tractorY==t.size() && !t.tractorX==0 && !t.tractorX == t.size()) {
+		}else if(t.tractorY()==t.size() && t.tractorX()!=0 && t.tractorX() != t.size()) {
 			//es decir, si el tractor esta en el borde de arriba pero no en una esquina
 			//de esta forma tendria tres sitios donde colocar tierra
 			//hacer que se pueda cojer y las combinaciones
