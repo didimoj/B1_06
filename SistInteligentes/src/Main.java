08215cf296fd23f6c94ab0b6ef40f5fb0ebe1556
didimoj <didimoj@hotmail.com>
2017-10-21 13:58:35 +0200

    Hito 2 hecho

--- SistInteligentes/src/Main.java
+++ SistInteligentes/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		gestionArchivo yo=new gestionArchivo();
 		Estado estInicial=yo.cargarArchivo("terreno.txt");
+		
 		Agente a=new Agente();
 		try {
 			a.getSolucion(estInicial);
