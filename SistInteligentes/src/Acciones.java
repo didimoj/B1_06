4b43cea820ea7b4cacf676ae45aa1fa383d018e4
didimoj <didimoj@hotmail.com>
2017-11-05 19:53:45 +0100

    Cambios importantes

--- SistInteligentes/src/Acciones.java
+++ SistInteligentes/src/Acciones.java
@@ -1,12 +1,15 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Acciones {
 	private int mov[];
 	private int dist[];
-	public Acciones(int[] m, int[] d) {
+	private ArrayList<int[]> casillas;
+	public Acciones(int[] m, int[] d,ArrayList<int[]> l) {
 		mov=m;
 		
 		dist=d;
+		casillas=l;
 	}
 	
 	public int[] getMov() {
@@ -27,6 +30,9 @@ public class Acciones {
 
 	@Override
 	public String toString() {
-		return "Acciones [Movimiento=" + Arrays.toString(mov) + ", Distribucion=" + Arrays.toString(dist) + "], Coste: 1";
+		String s="";
+		for(int i=0;i<casillas.size();i++)
+			s+=dist[i]+",("+Arrays.toString(casillas.get(i))+") ";
+		return "Acciones [Movimiento=" + Arrays.toString(mov) + ", Distribucion= [ " + s + "] , Coste: 1";
 	}
 }
